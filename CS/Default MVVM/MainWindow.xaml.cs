57908f509f9f6dc1c725f2c00537da683de9b60d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-10-25 20:46:52 +0300

    initial commit

---
+++ CS/Default MVVM/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DXWpfApplication;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Bars;
+using System.Windows.Markup;
+using DevExpress.Xpf.Editors;
+using System.Windows.Media.Animation;
+
+namespace Default_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TestDataViewsModel(); 
+        }
+
+        void OnShowGridMenu(object sender, GridMenuEventArgs e)
+        {
+            if (e.MenuType == GridMenuType.Column)
+            {
+                GridColumnHeader columnHeader = e.TargetElement as GridColumnHeader;
+                GridColumn column = columnHeader.DataContext as GridColumn;
+                bool showColumnHeaderEditor = ColumnBehavior.GetIsRenameEditorActivated(column);
+                BarButtonItem item = new BarButtonItem();
+                if (showColumnHeaderEditor)
+                    item.Content = "Hide ColumnHeader Editor";
+                else
+                    item.Content = "Show ColumnHeader Editor";
+                item.ItemClick += OnItemClick;
+                item.Tag = column;
+                e.Customizations.Add(item);
+            }
+        }
+
+        static void OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            GridColumn column = e.Item.Tag as GridColumn;
+            ColumnBehavior.SetIsRenameEditorActivated(column, !ColumnBehavior.GetIsRenameEditorActivated(column));
+        }
+
+        private void OnRenameEditorLostFocus(object sender, RoutedEventArgs e)
+        {
+            TextEdit edit = sender as TextEdit;
+            edit.Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+   
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# GridControl - How to rename column headers at runtime
+
+
+<p>This example illustrates how to rename column headers at runtime by setting the  <a href="http://documentation.devexpress.com/#WPF/DevExpressXpfGridDataViewBase_ColumnHeaderTemplatetopic"><u>DataViewBase.ColumnHeaderTemplate property</u></a>.  </p><p>To activate this feature, simply invoke the <strong>Column Header context menu</strong> and choose the <strong>Show ColumnHeader Editor</strong> item. Then, TextEdit will be shown within a corresponding column header. This editor gets hidden when it loses focus. If this editor was not focused, and you wish to hide it, invoke the Column Header context menu and choose the <strong>Hide ColumnHeader Editor</strong> item.</p>
+
+<br/>
+
+
